5263d1d0159fa9c2f1b4a0cb9d4dcc09639b4e3b
DzmitryZ <48861240+bruhzinga@users.noreply.github.com>
2023-08-07 22:00:20 +0300

    ...

--- PSKP/LABA31/31.ts
+++ PSKP/LABA31/31.ts
@@ -1,6 +1,6 @@
 import TelegramBot from "node-telegram-bot-api";
 // replace the value below with the Telegram token you receive from @BotFather
-const token = '6114999175:AAEU2kDHz5WIar7LCoeA_henGP7tJirjepk';
+const token = 'token';
 
 const bot = new TelegramBot(token, {polling: true});
 
